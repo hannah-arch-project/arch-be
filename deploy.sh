cfe55001d300f59c87b10b294bf045dba45674df
EC2 Default User <ec2-user@ip-10-0-0-6.ap-northeast-2.compute.internal>
2025-03-28 07:58:14 +0000

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/ec2-user/app
+
+# 예: 워드프레스라면 설정 등 재시작 또는 복사 가능
+# php, nginx 재시작 등
+
+echo "배포 완료"
---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo yum install -y nodejs npm || true
+cd /home/ec2-user/app
+npm install
